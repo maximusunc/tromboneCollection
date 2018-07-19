e388c5fb21b977325655c036a7ad9a38a2b2cad0
Max Wang <maxwellswang@gmail.com>
2018-07-19 13:36:16 -0400

    got background image to show

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -7,10 +7,10 @@ const Contact = props =>
             This is the page where you can submit edits or new instruments.
         </p>
         <p>
-            Click the following link to download our contact form with all the information on the new instrument or edit: <a href="./images/trombones/trombone.jpeg" download="instrument_form">Form</a>
+            Click the following link to download our contact form with all the information on the new instrument or edit: <a className="contactLink" href="./images/trombones/trombone.jpeg" download="instrument_form">Contact Form</a>
         </p>
         <p>
-            Once you've filled out the form, attach it and email to <a href="mailto:mkris@email.unc.edu">mkris@email.unc.edu</a> so we can review it. Thank you!
+            Once you've filled out the form, attach it and email to <a className="contactLink" href="mailto:mkris@email.unc.edu">mkris@email.unc.edu</a> so we can review it. Thank you!
         </p>
     </Container>;
 
