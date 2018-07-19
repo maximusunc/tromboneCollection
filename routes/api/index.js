d35dea3ef45efadd094d4a2afc41686ce935e925
Max Wang <maxwellswang@gmail.com>
2018-07-19 13:49:37 -0400

    got rid of unnecessary api route files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const tromboneRoutes = require("./tromboneRoutes");
-const jsonRoutes = require("./jsonRoutes");
 
 router.use("/trombones", tromboneRoutes);
 
-router.use("/details", jsonRoutes);
 
 module.exports = router;
--- routes/api/jsonRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const tromboneController = require("../../controllers/tromboneController.js");
-    
-// matches with /api/details/:id
-router.route("/:id")
-    .get(tromboneController.getTrombone);
-
-
-module.exports = router;
