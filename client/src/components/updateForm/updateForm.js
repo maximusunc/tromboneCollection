da665c60d75e69c7c8194c523f09059ff238c62f
Max Wang <maxwellswang@gmail.com>
2018-09-27 18:55:42 -0400

    storing images in mongodb

--- client/src/components/updateForm/updateForm.js
+++ client/src/components/updateForm/updateForm.js
@@ -1,89 +1,119 @@
 import React from "react";
 import "./updateForm.css";
+import Footnotes from "../footnotes";
 
-const UpdateForm = (props) => 
-    <div>
-        <form className="col s12 search" encType="multipart/form-data">
-            <div className="row">
-                <div className="input-field col s4">
-                    <input id="maker" name="maker" type="text" className="active" value={props.maker || ""} onChange={props.onChange} required />
-                    <label className="active" htmlFor="maker">Maker</label>
+const UpdateForm = (props) => {
+    const footnotes = props.footnotes.map((footnote, index) => (
+        <Footnotes
+            key={index}
+            id={index}
+            footnote={footnote}
+            handleUpdate={props.handleFootnotes}
+        />
+    ));
+    return (
+        <div>
+            <form className="col s12 search" encType="multipart/form-data">
+                <div className="row">
+                    <div className="input-field col s4">
+                        <input id="maker" name="maker" type="text" className="active" value={props.maker || ""} onChange={props.onChange} required />
+                        <label className="active" htmlFor="maker">Maker</label>
+                    </div>
+                    <div className="input-field col s4">
+                        <input id="date" name="date" type="text" className="active" value={props.date || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="date">Date</label>
+                    </div>
+                    <div className="input-field col s4">
+                        <select name="type" className="browser-default" value={props.type || ""} onChange={props.onChange}>
+                            <option value=""></option>
+                            <option value="Alto">Alto</option>
+                            <option value="Tenor">Tenor</option>
+                            <option value="Bass">Bass</option>
+                            <option value="Sackbut">Sackbut</option>
+                        </select>
+                        <label className="active">Type</label>
+                    </div>
                 </div>
-                <div className="input-field col s4">
-                    <input id="date" name="date" type="text" className="active" value={props.date || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="date">Date</label>
+                <div className="row">
+                    <div className="input-field col s4">
+                        <input id="location" name="location" type="text" className="active" value={props.location || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="location">Location</label>
+                    </div>
+                    <div className="input-field col s4">
+                        <input id="signature" name="signature" type="text" className="active" value={props.signature || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="signature">Signature</label>
+                    </div>
+                    <div className="input-field col s4">
+                        <input id="pitch" name="pitch" type="text" className="active" value={props.pitch || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="pitch">Pitch</label>
+                    </div>
                 </div>
-                <div className="input-field col s4">
-                    <select name="type" className="browser-default" value={props.type || ""} onChange={props.onChange}>
-                        <option value=""></option>
-                        <option value="Alto">Alto</option>
-                        <option value="Tenor">Tenor</option>
-                        <option value="Bass">Bass</option>
-                        <option value="Sackbut">Sackbut</option>
-                    </select>
-                    <label className="active">Type</label>
+                <div className="row">
+                    <div className="input-field col s4">
+                        <input id="dimensions" name="dimensions" type="text" className="active" value={props.dimensions || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="dimensions">Dimensions</label>
+                    </div>
+                    <div className="input-field col s4">
+                        <input id="provenance" name="provenance" type="text" className="active" value={props.provenance || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="provenance">Provenance</label>
+                    </div>
+                    <div className="input-field col s4">
+                        <input id="mouthpiece" name="mouthpiece" type="text" className="active" value={props.mouthpiece || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="mouthpiece">Mouthpiece</label>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s4">
-                    <input id="location" name="location" type="text" className="active" value={props.location || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="location">Location</label>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <textarea id="literature" name="literature" type="text" className="active large-textarea" value={props.literature || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="literature">Literature</label>
+                    </div>
                 </div>
-                <div className="input-field col s4">
-                    <input id="signature" name="signature" type="text" className="active" value={props.signature || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="signature">Signature</label>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <textarea id="remarks" name="remarks" type="text" className="active large-textarea" value={props.remarks || ""} onChange={props.onChange} />
+                        <label className="active" htmlFor="remarks">Remarks</label>
+                    </div>
                 </div>
-                <div className="input-field col s4">
-                    <input id="pitch" name="pitch" type="text" className="active" value={props.pitch || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="pitch">Pitch</label>
-                </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s4">
-                    <input id="dimensions" name="dimensions" type="text" className="active" value={props.dimensions || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="dimensions">Dimensions</label>
-                </div>
-                <div className="input-field col s4">
-                    <input id="found" name="found" type="text" className="active" value={props.found || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="found">Found</label>
-                </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s12">
-                    <textarea id="literature" name="literature" type="text" className="active" value={props.literature || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="literature">Literature</label>
-                </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s12">
-                    <textarea id="remarks" name="remarks" type="text" className="active" value={props.remarks || ""} onChange={props.onChange} />
-                    <label className="active" htmlFor="remarks">Remarks</label>
-                </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s12">
-                    <textarea id="footnotes" name="footnotes" type="text" className="active" value={props.footnotes || ""} onChange={props.onChange} />
+                <div className="row" id="footnotes">
                     <label className="active" htmlFor="footnotes">Foot Notes</label>
+                    {footnotes}
                 </div>
-            </div>
-            <div className="row">
-                <div className="file-field input-field">
-                    <div className="btn">
-                        <span>Upload Image</span>
-                        <input ref={(ref) => {props.file = ref}} type="file" accept="image/*" />
-                    </div>
-                    <div className="file-path-wrapper">
-                        <input name="imagePath" className="file-path validate" type="text" />
-                    </div>
+                <button id="newFootnote" onClick={props.handleNewFootnote}>New Footnote</button>
+                <div className="row">
+                    {props.image && props.image.length > 4 ?
+                        <div>
+                            <img src={props.image} alt="tromboneImage" />
+                            <div className="file-field input-field">
+                                <div className="btn">
+                                    <span>Change Image</span>
+                                    <input id="fileUpload" type="file" accept="image/*" onChange={props.fileUpload} />
+                                </div>
+                                <div className="file-path-wrapper">
+                                    <input name="imagePath" className="file-path validate" type="text" />
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        <div className="file-field input-field">
+                            <div className="btn">
+                                <span>Upload Image</span>
+                                <input id="fileUpload" type="file" accept="image/*" onChange={props.fileUpload} />
+                            </div>
+                            <div className="file-path-wrapper">
+                                <input name="imagePath" className="file-path validate" type="text" />
+                            </div>
+                        </div>
+                    }
                 </div>
-            </div>
-            <div className="row">
-                <button id="createTrombone" type="submit" onSubmit={props.handleSubmit}>{props.button}</button>
-                {props.delete ? (
-                    <button id="delete" onClick={props.delete}>Delete</button>
-                ): (null)}
-            </div>
-        </form>
-    </div>;
+                <div className="row">
+                    <button id="createTrombone" type="button" onClick={props.handleSubmit}>{props.button}</button>
+                    {props.delete ? (
+                        <button id="delete" type="button" onClick={props.delete}>Delete</button>
+                    ) : (null)}
+                </div>
+            </form>
+        </div>
+    );
+};
 
 export default UpdateForm;
