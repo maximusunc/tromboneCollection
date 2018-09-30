a088669e9bc568b642bee38f3ed7398f8a8d7d8e
Max Wang <maxwellswang@gmail.com>
2018-09-30 16:14:40 -0400

    fixed image display bug, other small adjustments

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./footer.css";
+import logo from "../../images/AMISBanner.jpg";
 
 const Footer = () =>
     <footer>
-      <img id="AMIS" src="./images/AMISBanner.jpg" alt="American Musical Instrument Society" />
+      <img id="AMIS" src={logo} alt="American Musical Instrument Society" />
       <h5>&copy; Trombone Collection 2018</h5>
     </footer>;
 
--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "../components/container";
+import tromboneForm from "../images/historicTromboneForm.pdf";
 
 const Contact = props => 
     <Container>
@@ -7,7 +8,7 @@ const Contact = props =>
             This is the page where you can submit edits or new instruments.
         </p>
         <p>
-            Click the following link to download the information form for edits and additions: <a className="contactLink" href="./images/historicTromboneForm.pdf" download="HistoricTromboneForm.pdf">Contact Form</a>
+            Click the following link to download the information form for edits and additions: <a className="contactLink" href={tromboneForm} download="HistoricTromboneForm.pdf">Contact Form</a>
         </p>
         <p>
             Once you've filled out the form, attach it and email to <a className="contactLink" href="mailto:mkris@email.unc.edu">mkris@email.unc.edu</a> so we can review it. Thank you!
