eac1e372774dcb2da88f917932d904b12dd910de
Max Wang <maxwellswang@gmail.com>
2018-11-06 18:22:02 -0500

    trying to set no caching

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ mongoose.connect(
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+  res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+  res.setHeader("Expires", "0"); // Proxies.
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
