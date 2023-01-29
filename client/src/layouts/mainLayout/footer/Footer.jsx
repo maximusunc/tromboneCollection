cc40ed6040b9a3110a7162925b4304b8f1381652
Max Wang <max@covar.com>
2023-01-28 21:27:41 -0500

    Add mircat banner

--- client/src/layouts/mainLayout/footer/Footer.jsx
+++ client/src/layouts/mainLayout/footer/Footer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./footer.css";
 import logo from "./AMISBanner.jpg";
+import mircat from "./mircat.png";
 
 const Footer = () =>
     <footer>
-      <img id="AMIS" src={logo} alt="American Musical Instrument Society" />
-      <p id="copyright">&copy; Trombone Collection 2018-2021</p>
+      <div style={{ marginTop: 10 }}>
+        <a href="https://mircat.org/" target="_blank">
+          <img id="MIRcat" src={mircat} alt="MIRcat Musical Instrument Research Catalog" />
+        </a>
+        <a href="https://www.amis.org/" target="_blank">
+          <img id="AMIS" src={logo} alt="American Musical Instrument Society" />
+        </a>
+      </div>
+      <p id="copyright">&copy; Trombone Collection 2018-2023</p>
     </footer>;
 
 export default Footer;
